0ba319ede7bebbcc62a984b79fe92a7b0e86ce3c
QoooQooo <withluna80@naver.com>
2023-12-21 21:47:23 +0900

    account

---
+++ resources/views/front/account/login.blade.php
@@ -0,0 +1,13 @@
+@extends('front.layouts.app')
+
+@section('customCss')
+
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('customJs')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\FrontController;
@@ -49,6 +50,10 @@ Route::get('/cart', [CartController::class, 'cart'])->name('front.cart');
 Route::post('/add-to-cart', [CartController::class, 'addToCart'])->name('front.addToCart');
 Route::post('/update-cart', [CartController::class, 'updateCart'])->name('front.updateCart');
 Route::post('/delete-item', [CartController::class, 'deleteItem'])->name('front.deleteItem.cart');
+
+
+//회원가입연결
+Route::get('/register', [AuthController::class, 'register'])->name('account.register');
 
 
 //관리자 페이지 연결설정
